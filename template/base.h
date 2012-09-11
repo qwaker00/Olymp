047be10d309791bea36b3b73214efa33a23f6927
Eugene Gritskevich <qwaker.00@gmail.com>
2012-09-11 14:59:03 +0400

    Added basic templates

---
+++ template/base.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdlib>
+#include <cassert>
+
+namespace qwaker {
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef unsigned int uint;
+
+}
---
+++ template/random.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <cstdlib>
+#include "base.h"
+
+namespace qwaker {
+
+int rand32() {
+    return (int(rand()) << 20) ^ (int(rand()) << 10) ^ int(rand());
+}
+
+LL rand64() {
+    return (LL(rand32()) << 32) ^ LL(rand32());
+}
+
+uint randu32() {
+    return (uint(rand()) << 20) ^ (uint(rand()) << 10) ^ uint(rand());
+}
+
+ULL randu64() {
+    return (ULL(randu32()) << 32) ^ ULL(randu32());
+}
+
+bool flip_coin(const uint a, const uint b) {
+    return randu32() % (a + b) < a;
+}
+
+int randint(int l, int r) {
+    return randu32() % (r - l + 1) + l;
+}
+
+double randfloat() {
+    return LD(randu64()) / (1ll << 32) / (1ll << 32);
+}
+
+}
